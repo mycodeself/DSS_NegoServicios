e8977d0c6adc87a98122a9198184ef3fc6e7c834
javier bellver <jbellver271@gmail.com>
2015-05-11 10:00:58 +0100

    Añadidas Interfaces Reservas Y Vuelos

---
+++ NegoServiciosGen/NegoServiciosGenNHibernate/UI/Reservas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NegoServiciosGenNHibernate.UI
+{
+    public partial class Reservas : Form
+    {
+        public Reservas()
+        {
+            InitializeComponent();
+        }
+    }
+}
