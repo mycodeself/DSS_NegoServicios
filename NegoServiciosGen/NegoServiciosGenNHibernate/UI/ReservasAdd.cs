227072962bdcae05f437b79ccdf6aa75b81423fc
javier bellver <jbellver271@gmail.com>
2015-05-17 14:09:48 +0100

    Añadido control de validacion reservas (again)

--- NegoServiciosGen/NegoServiciosGenNHibernate/UI/ReservasAdd.cs
+++ NegoServiciosGen/NegoServiciosGenNHibernate/UI/ReservasAdd.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using NegoServiciosGenNHibernate.CEN.NegoServicios;
+using NegoServiciosGenNHibernate.CAD.NegoServicios;
 using NegoServiciosGenNHibernate.Enumerated.NegoServicios;
+using NegoServiciosGenNHibernate.EN.NegoServicios;
 
 namespace NegoServiciosGenNHibernate.UI
 {
@@ -28,18 +30,56 @@ namespace NegoServiciosGenNHibernate.UI
             try
             {
                 ReservaCEN cen = new ReservaCEN();
-                string aux_estado=estado_textbox.Text,aux_id_cliente,aux_id_vuelo,aux_plazas;
+                string aux_estado=estado_textbox.Text,aux_id_cliente,aux_id_vuelo,aux_plazas,validacion;
                 EstadoReservaEnum estado;
-                estado = (EstadoReservaEnum)Enum.Parse(typeof(EstadoReservaEnum), aux_estado);
-                aux_id_cliente = this.id_cliente_textbox.Text;
-                aux_id_vuelo = this.id_vuelo_textbox.Text;
-                aux_plazas = this.plazas_textbox.Text;
-                cen.New_(Convert.ToInt32(aux_id_cliente),Convert.ToInt32(aux_id_vuelo),Convert.ToInt32(aux_plazas),estado);
+                try
+                {
+                    estado = (EstadoReservaEnum)Enum.Parse(typeof(EstadoReservaEnum), aux_estado);
+                    aux_id_cliente = this.id_cliente_textbox.Text;
+                    aux_id_vuelo = this.id_vuelo_textbox.Text;
+                    aux_plazas = this.plazas_textbox.Text;
+                    validacion = validar(aux_id_cliente, aux_id_vuelo, aux_plazas, estado);
+                    if (validacion == "")
+                    {
+                        cen.New_(Convert.ToInt32(aux_id_cliente), Convert.ToInt32(aux_id_vuelo), Convert.ToInt32(aux_plazas), estado);
+                    }
+                    else
+                    {
+                        MessageBox.Show(validacion);
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show("Error, tipo de estado incorrecto");
+                }
             }
             catch (NegoServiciosGenNHibernate.Exceptions.DataLayerException ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        public string validar(string id_cliente, string id_vuelo, string plazas, EstadoReservaEnum estado)
+        {
+            string validacion = "";
+            ClienteCAD cad_cliente = new ClienteCAD();
+            VueloCAD cad_vuelo = new VueloCAD();
+            try
+            {
+                ClienteEN cliente = new ClienteEN(cad_cliente.ReadOIDDefault(Convert.ToInt32(id_cliente)));
+                VueloEN vuelo = new VueloEN((cad_vuelo.ReadOIDDefault(Convert.ToInt32(id_vuelo))));
+                if (Convert.ToInt32(plazas) > vuelo.Plazas)
+                {
+                    validacion = "Número de plazas a reservar incorrecto";
+                }
+            }
+            catch(NegoServiciosGenNHibernate.Exceptions.DataLayerException ex)
+            {
+                validacion = "Cliente ID y Vuelo ID incorrectos \n";
+            }
+
+
+            return validacion;
         }
     }
 }
